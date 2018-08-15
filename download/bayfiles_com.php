62c216195962181bcbc3c6be2c6695e992b54927
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-08-16 01:08:02 +0430

    Add files via upload

---
+++ download/bayfiles_com.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('RAPIDLEECH')) {
+    require_once('index.html');
+    exit;
+}
+
+class bayfiles_com extends DownloadClass {
+    public function Download($link) {
+        $page = $this->GetPage($link);
+        if(!preg_match('/<a type="button" id="download-url" class="btn btn-primary btn-block" href="(.*?)">/', $page, $download_link)) html_error('File not found!');
+        $this->RedirectDownload($download_link[1], 0, 0, 0, $link);
+    }
+}
+
+// [16-08-2018] Written by NimaH79.
