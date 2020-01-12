0088ec1b03a430cb63585404a2025eea3e7e60a4
Nima HeydariNasab <nima.heydari79@yahoo.com>
2020-01-12 17:55:08 +0330

    Fix bayfiles.com

--- download/bayfiles_com.php
+++ download/bayfiles_com.php
@@ -10,7 +10,7 @@ class bayfiles_com extends DownloadClass
     public function Download($link)
     {
         $page = $this->GetPage($link);
-        if (!preg_match('/<a type="button" id="download-url" class="btn btn-primary btn-block" href="(.*?)">/', $page, $download_link)) {
+        if (!preg_match('/<a type="button" id="download-url"[\s\n]+class="btn btn-primary btn-block"[\s\n]+href="(.*?)">/', $page, $download_link)) {
             html_error('File not found!');
         }
         $this->RedirectDownload($download_link[1], 0, 0, 0, $link);
@@ -18,3 +18,4 @@ class bayfiles_com extends DownloadClass
 }
 
 // [16-08-2018] Written by NimaH79.
+// [12-01-2020] Fixed by NimaH79.
