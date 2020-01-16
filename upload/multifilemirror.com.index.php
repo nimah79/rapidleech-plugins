309045732bb1161c75b81c3729d89fcfea1824e1
Nima HeydariNasab <nima.heydari79@yahoo.com>
2020-01-16 19:48:36 +0330

    Add multifilemirror.com upload plugin

---
+++ upload/multifilemirror.com.index.php
@@ -0,0 +1,5 @@
+<?php
+
+$upload_services[] = 'multifilemirror.com';
+$max_file_size['multifilemirror.com'] = 2050;
+$page_upload['multifilemirror.com'] = 'multifilemirror.com.php';
---
+++ upload/multifilemirror.com.php
@@ -0,0 +1,46 @@
+<?php
+
+echo '<center>multifilemirror.com plugin by <b>NimaH79</b></center><br>';
+
+if (version_compare(PHP_VERSION, '5.5', '<')) {
+    html_error('You should have PHP 5.5 or higher.');
+}
+
+echo "<table style='width:600px;margin:auto;'>\n<tr><td align='center'>";
+
+$ch = curl_init('https://multifilemirror.com/');
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false,
+]);
+$response = curl_exec($ch);
+curl_close($ch);
+preg_match('/action="(.*?)"/', $response, $action);
+$action = $action[1];
+preg_match('/upload_id=(.*)/', $action, $upload_id);
+$upload_id = $upload_id[1];
+$url = parse_url($action);
+$pfile = upfile($url['host'], 0, $url['path'].($url['query'] ? '?'.$url['query'] : ''), 0, 0, [
+        'u_hash' => '',
+        'remote' => '0',
+        'upload_id' => $upload_id,
+        'fileuploader-list-file0' => json_encode([['file' => '0:/' . $lname]]),
+        'filepass' => '',
+        'tabs' => 'on',
+    ], $lfile, $lname, 'file0');
+is_page($pfile);
+preg_match('/({.*?})/', $pfile, $response);
+$response = json_decode($response[1], true);
+$ch = curl_init('https://multifilemirror.com/result/'.$response['code']);
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false,
+]);
+$response = curl_exec($ch);
+curl_close($ch);
+preg_match('/<textarea id="fullurl".*?>(.*?)&#13;&#10;</', $response, $download_link);
+$download_link = str_replace(' ', '%20', $download_link[1]);
+
+// [16-01-2020] - Written by NiamH79
