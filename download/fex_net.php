40da4b90c1927ba6ffba0bed3be3951e9f03c95f
Nima HeydariNasab <nima.heydari79@yahoo.com>
2020-09-17 00:27:02 +0430

    Add fex.net download plugin

---
+++ download/fex_net.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!defined('RAPIDLEECH')) {
+    require_once('index.html');
+    exit;
+}
+
+class fex_net extends DownloadClass {
+    public function Download($link) {
+        if (!preg_match('/\/s\/(.*)$/', $link, $slug)) {
+            html_error('Invalid URL');
+        }
+        $slug = $slug[1];
+        $token = $this->GetPage('https://api.fex.net/api/v1/anonymous/upload-token');
+        if (!preg_match('/"token":"(.*?)"/', $token, $token)) {
+            html_error('Cannot get token');
+        }
+        $token = $token[1];
+        $id = $this->GetPage('https://api.fex.net/api/v2/file/share/'.$slug);
+        if (!preg_match('/"id":(\d+),"is_dir/', $id, $id)) {
+            html_error('Cannot get ID');
+        }
+        $id = $id[1];
+        $info = $this->GetPage('https://api.fex.net/api/v2/file/share/children/'.$slug.'/'.$id);
+        if (!preg_match('/"download_url":"(.*?)"/', $info, $dlink)) {
+            html_error('File not found');
+        }
+        $dlink = $dlink[1];
+        preg_match('/"name":"(.*?)",/', $info, $FileName);
+        $FileName = json_decode('"'.$FileName[1].'"');
+        $cookie = 'token='.$token;
+        $this->RedirectDownload($dlink, $FileName, $cookie, 0, $link);
+    }
+}
+
+// [17-09-2020] Written by NimaH79.
